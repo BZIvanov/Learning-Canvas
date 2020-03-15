01dbb230e56f75818f015c2b8bb6799ed3b983a9
BZIvanov <biser.z.ivanov@gmail.com>
2020-03-15 17:29:41 +0200

    transformations

---
+++ 06.Events/scripts.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  // 01. SCALE ------------------------------------------------------------------
+
+  const cv1 = this.document.getElementById('1');
+  const ctx1 = cv1.getContext('2d');
+  cv1.width = 600;
+  cv1.height = 300;
+
+  
+};
